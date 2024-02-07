d00753842930f32f840723fc8ca1211f7131f6be
Angel Vargas <angelvargas@outlook.es>
2024-02-07 00:55:42 +0000

    stack(node): dependencies updated to run on node 18.

--- saas_app/beta/models.py
+++ saas_app/beta/models.py
@@ -1,19 +1,24 @@
+from model_utils.models import TimeStampedModel
+
 from django.db import models
 from django.utils.crypto import get_random_string
-from model_utils.models import TimeStampedModel
-from saas_app.models.base import BaseModel
+
 
 def get_code():
     return get_random_string(50)
 
-class Invite(BaseModel):
+
+class Invite(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
     code = models.CharField(max_length=50, default=get_code)
     request = models.OneToOneField("Request", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.code
 
-class Request(BaseModel):
+
+class Request(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
 
     def __str__(self):
