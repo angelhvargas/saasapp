70dfe7e0bcc79d9a0734d19f705707593851c371
Angel Vargas <angelvargas@outlook.es>
2024-02-07 02:40:10 +0000

    bugfix(email): adding missing receivers

--- saas_app/users/signals.py
+++ saas_app/users/signals.py
@@ -1,19 +1,22 @@
-# saas_app/users/signals.py
-
 from django.core.mail import send_mail
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+# Assuming top-level import doesn't cause cyclic import issues
+from saas_app.users.tasks import subscribe_to_mailing_list
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def send_registration_mail(sender, instance, created, **kwargs):
     if created:
         send_mail(
             "New Registration",
-            message="{email} has just registered".format(email=instance.email),
-            from_email=getattr(settings, "DEFAULT_FROM_EMAIL"),
+            message=f"{instance.email} has just registered",
+            from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[settings.SAAS_INFO_MAIL],
         )
 
-def subscribe_to_mailing_list(sender, instance, created, **kwargs):
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def subscribe_user_to_mailing_list(sender, instance, created, **kwargs):
     if created:
-        # Local import within the signal handler
-        from .tasks import subscribe_to_mailing_list
         subscribe_to_mailing_list.delay(user_pk=instance.pk)
